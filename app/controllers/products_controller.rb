0103d4ede8e0e1a752b27d9371b0f18c8d1a94d0
Michael Anderson <michanders@msn.com>
2018-02-21 19:44:39 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,16 @@ class ProductsController < ApplicationController
   def create
     @product = Product.find_or_create_by(prod_params)
     redirect to products_path
+  end
 
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
   end
 
 
